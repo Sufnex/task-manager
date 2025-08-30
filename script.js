183cb17c12076dd9eb10e5e0151db65f5d2f751c
Sufnex <39780764+Sufnex@users.noreply.github.com>
2025-08-30 21:31:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
+auth.useDeviceLanguage();
 console.log('✅ Firebase inicializálva:', app);
 
 // Globális változók
@@ -881,3 +885,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
   console.log('🚀 Multi-User Task Manager betöltve!');
 });
+
